0e4379f3521d14847e9f6ee900b4ffdb83204cac
anastasiazzz <anastasia.papko7@gmail.com>
2017-03-15 09:33:18 +0300

    Class Point, Fuction distance are added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,17 +3,16 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.y = 4;
-    p1.x = 22;
+    Point p1 = new Point(4,22);
 
-    Point p2 = new Point();
-    p2.y = 8;
-    p2.x = 25;
+    Point p2 = new Point(8,25);
+
     System.out.println("Расстояние между двумя точками с координатами(x,y): "+"("+p2.x+";"+p2.y+")"+" и "+"("+p1.x+";"+p1.y+")"+" равно "+distance(p1,p2));
 
     }
-  public static double distance(Point p1, Point p2) {
+  public static double distance(Point p1,Point p2) {
+
     return Math.sqrt((p2.y-p1.y)*(p2.y-p1.y)+(p2.x-p1.x)*(p2.x-p1.x));
   }
+
 }
