9ba0015ef85d253732c9d4981e424a26d6182287
anastasiazzz <anastasia.papko7@gmail.com>
2017-03-15 09:43:58 +0300

    Function distance is moved to Class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,13 +6,10 @@ public class MyFirstProgram {
     Point p1 = new Point(4,22);
 
     Point p2 = new Point(8,25);
-
-    System.out.println("Расстояние между двумя точками с координатами(x,y): "+"("+p2.x+";"+p2.y+")"+" и "+"("+p1.x+";"+p1.y+")"+" равно "+distance(p1,p2));
+    System.out.println("Расстояние между двумя точками с координатами(x,y): "+"("+p2.x+";"+p2.y+")"+" и "+"("+p1.x+";"+p1.y+")"+" равно "+p1.distance(p1,p2));
+   // System.out.println("Расстояние между двумя точками с координатами(x,y): "+"("+p2.x+";"+p2.y+")"+" и "+"("+p1.x+";"+p1.y+")"+" равно "+distance(p1,p2));
 
     }
-  public static double distance(Point p1,Point p2) {
 
-    return Math.sqrt((p2.y-p1.y)*(p2.y-p1.y)+(p2.x-p1.x)*(p2.x-p1.x));
-  }
 
 }
