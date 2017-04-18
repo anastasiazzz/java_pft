40f840f967da416eb2fdee3c993cadc4a12b00b1
anastasiazzz <anastasia.papko7@gmail.com>
2017-04-18 09:21:08 +0300

    Fluent-style checks are added to both Contacts and Group tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactUpdationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactUpdationTests.java
@@ -1,13 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.assertEquals;
 
 public class ContactUpdationTests extends TestBase{
 
@@ -20,17 +27,15 @@ public class ContactUpdationTests extends TestBase{
   }
   @Test
   public void testContactUpdation() {
-   Set<ContactData> before = app.contact().all();
+   Contacts before = app.contact().all();
     ContactData modifiedContact = before.iterator().next();
     ContactData contact = new ContactData()
             .withId(modifiedContact.getId()).withFirstName("firstName3").withMiddleName("middleNameUPDATED").withLastName("lastNameUPDATED").withNickName("NickNameUPDATED").withCompany("CompanyUPDATED").withAddress("AddressUPDATED").withHomePhone("TelephoneUPDATED").withEmail("emailUPDATED").withGroup( "test1");
     app.contact().modify(contact);
     app.returnToHomePage();
-    Set<ContactData> after = app.contact().all();
-
-    before.remove(modifiedContact);
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    Contacts after = app.contact().all();
+    assertEquals(after.size(), before.size());
+    assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
 
   }
 }
